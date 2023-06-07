49cb0e122ac26b055a5d1a9047834155b7fb6883
Celia Rubio <celia_rubio@hotmail.es>
2023-06-07 17:14:02 +0100

    bumpy lines

--- BasicDiagram.hs
+++ BasicDiagram.hs
@@ -21,7 +21,7 @@ drawDiagram Morphism =
 
 drawDiagram (Compose sd1 sd2) =
     (unitSquare # opacity 0 <> 
-    (d1 # scaleX 0.5 ||| d2 # scaleX 0.5) # centerXY, 
+    (d1 # scale 0.5 ||| d2 # scale 0.5) # centerXY, 
     (in1,out2))
     where
         (d1,(in1,out1)) = drawDiagram sd1
@@ -29,15 +29,33 @@ drawDiagram (Compose sd1 sd2) =
 
 drawDiagram (Tensor sd1 sd2) =
     (unitSquare # opacity 0 <> 
-    (d1 # scaleY 0.5 === d2 # scaleY 0.5) # centerXY, 
+    d <> mconcat (extendLeft ++ extendRight), 
     (in1 + in2, out1 + out2))
     where
         (d1,(in1,out1)) = drawDiagram sd1
         (d2,(in2,out2)) = drawDiagram sd2
+        d = (d1 # scale 0.5 === d2 # scale 0.5) # centerXY
+        extendLeft = extendLines in1 in2 (-0.25) (-0.5)
+        extendRight = extendLines out1 out2 0.25 0.5
+
+extendLines :: Integer -> Integer -> Double -> Double -> [Diagram B]
+extendLines i1 i2 xS xE = trails
+    where
+        n1 = fromInteger i1
+        n2 = fromInteger i2
+        startP = [ p2 (xS,0.5-i*0.5/(n1+1)) | i<-[1..n1] ] ++ [ p2 (xS,-i*0.5/(n2+1)) | i<-[1..n2] ]
+        endP = [ p2 (xE,0.5-i/(n1+n2+1)) | i<-[1..n1+n2] ]
+        trails = map (\(s,e) -> fromVertices [s,e]) (zip startP endP)
         
+example :: StringDiagram
 example = Tensor (Compose Morphism Morphism) (Tensor Morphism Morphism)
 
 mainDiagram :: Diagram B
 mainDiagram = unitSquare <> (fst $ drawDiagram example)
 
 main = mainWith mainDiagram
+
+-- TODOS
+-- line width
+-- box size
+-- centerXY

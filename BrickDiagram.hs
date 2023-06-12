c4866883ef4d81df782f7cb9be21de8079d2ecf0
Celia Rubio <celia_rubio@hotmail.es>
2023-06-12 19:23:39 +0100

    brick diagrams

--- BrickDiagram.hs
+++ BrickDiagram.hs
@@ -4,86 +4,79 @@
 
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
-import Diagrams.TwoD.Align
+-- import Diagrams.TwoD.Align
+-- import Diagrams.Path
+-- import Diagrams.Segment
 
 data BrickDiagram = Morphism Arity | Compose BrickDiagram BrickDiagram | Tensor BrickDiagram BrickDiagram
 type Arity = (Integer, Integer)
 
-drawDiagram :: BrickDiagram -> (Diagram B, Arity)
+lineMN :: (Double,Double) -> (Double,Double) -> (Double,Double)
+lineMN (x1,y1) (x2,y2) = (slope,y1-slope*x1) where slope = (y2-y1)/(x2-x1)
 
-drawDiagram (Morphism (aL, aR)) = (unitSquare, (aL, aR))
+changeLine :: (P2 Double -> Bool) -> (P2 Double -> P2 Double) -> Path V2 Double -> Path V2 Double
+changeLine ifOnLine moveToNew path = toPath moveSegments
+    where 
+        cond p = if ifOnLine p then moveToNew p else p
+        getSegments = map (map mkFixedSeg) (pathLocSegments path)
+        moveSegments = map (map (\(FLinear p1 p2) -> FLinear (cond p1) (cond p2))) getSegments
 
-drawDiagram (Compose sd1 sd2) =
-    ((d1 # scaleX 0.5 ||| d2 # scaleX 0.5) # centerXY,
-    (a1L,a2R))
-    where
-        (d1,(a1L,a1R)) = drawDiagram sd1
-        (d2,(a2L,a2R)) = drawDiagram sd2
-
-drawDiagram (Tensor sd1 sd2) =
-    ((d1 # scaleY (fromInteger aL) ||| d2 # scaleX (fromInteger aR)) # centerXY,
-    (a1L,a2R))
-    where
-        (d1,(a1L,a1R)) = drawDiagram sd1
-        (d2,(a2L,a2R)) = drawDiagram sd2
-        (aL,aR) = (a1L+a2L,a1R+a2R)
-
-extendLines :: Integer -> Integer -> Double -> Double -> [Diagram B]
-extendLines i1 i2 xS xE = trails
-    where
-        n1 = fromInteger i1
-        n2 = fromInteger i2
-        startP = [ p2 (xS,0.5-i*0.5/(n1+1)) | i<-[1..n1] ] ++ [ p2 (xS,-i*0.5/(n2+1)) | i<-[1..n2] ]
-        endP = [ p2 (xE,0.5-i/(n1+n2+1)) | i<-[1..n1+n2] ]
-        trails = map (\(s,e) -> fromVertices [s,e]) (zip startP endP)
+changeTopLine :: (Double,Double) -> (Path V2 Double, Arity) -> Path V2 Double
+changeTopLine (m',n') (path, (al,ar)) = (top . left . right) path
+    where 
+        (m,n) = lineMN (0,fromInteger al) (width path,fromInteger ar)
+        ---
+        ifTop p = abs ((p^._y) - (m * (p^._x) + n)) <= 0.001
+        moveTop p = (p^._x) ^& (m' * (p^._x) + n')
+        top = changeLine ifTop moveTop
+        ----
+        ifLeft p = abs (p^._x) <= 0.001
+        moveLeft p = (p^._x) ^& ((p^._y)*n'/n)
+        left = changeLine ifLeft moveLeft
+        ----
+        ifRight p = abs ((p^._x) - width path) <= 0.001
+        moveRight p = (p^._x) ^& ((p^._y)*(m' * (p^._x) + n')/(fromInteger ar))
+        right = changeLine ifRight moveRight
         
+drawPath :: BrickDiagram -> (Path V2 Double, Arity)
+drawPath (Morphism (al,ar)) = 
+    (changeTopLine 
+        (lineMN (0,fromInteger al) (1,fromInteger ar)) 
+        (unitSquare # alignBL, 
+        (1,1)),
+    (al,ar))
+
+drawPath (Compose bd1 bd2) =
+    (newP1 ||| newP2,
+    (al1',ar2'))
+    where
+        d1@(p1,(al1',ar1')) = drawPath bd1
+        d2@(p2,(al2',ar2')) = drawPath bd2
+        [al1,ar1,al2,ar2] = map fromInteger [al1',ar1',al2',ar2']
+        [w1,w2] = map width [p1,p2]
+        middle = (al1+ar2)/2
+        (newP1,newP2) = if middle > ar1 then moveMiddle else moveSides
+        moveMiddle = 
+            (changeTopLine (lineMN (0,al1) (w1,middle)) d1,
+            changeTopLine (lineMN (0,middle) (w2,ar2)) d2)
+        moveSides =
+            (changeTopLine (lineMN (0,x*al1/ar2) (w2,ar1)) d1 # scaleY (ar2/x),
+            changeTopLine (lineMN (0,ar1) (w2,x)) d2 # scaleY (ar2/x))
+            where x = ar1 / (1 - w2*(1 - al1/ar2)/(w1+w2))
+
+drawPath (Tensor bd1 bd2) =
+    ((p1 # scaleX (maxW/w1) # shearY ((ar2-al2)/maxW) 
+    <> p2 # scaleX (maxW/w2) # snugT) # alignBL,
+    (al1'+al2',ar1'+ar2'))
+    where
+        (p1,(al1',ar1')) = drawPath bd1
+        (p2,(al2',ar2')) = drawPath bd2
+        [al1,ar1,al2,ar2] = map fromInteger [al1',ar1',al2',ar2']
+        [w1,w2] = map width [p1,p2]
+        maxW = max w1 w2 -- could take min?
+
 example :: BrickDiagram
 example = Compose (Tensor (Morphism (1, 1)) (Morphism (1, 2))) (Tensor (Morphism (2, 1)) (Morphism (1, 1)))
 
-data Side = LeftSide | RightSide
--- converts square into hill-like quadrilateral
--- f is stretch factor.
-wibble :: Side -> Double -> Deformation V2 V2 Double
-wibble LeftSide f = Deformation $ \p ->
-  (p^._x) ^& ((p^._y)+(p^._y)*(p^._x-1)*(1-f))
-wibble RightSide f = Deformation $ \p ->
-  (p^._x) ^& ((p^._y)*((p^._x)*x_0 + (1-(p^._x))*x_1))
-    where 
-        x_0 = 1
-        x_1 = f
-
-
--- mylines :: Path V2 Double
--- mylines = mconcat $ [p2 (i,0) ~~ p2 (i,1) | i<-[0,0.1..1]] # alignBL
-mylines :: Path V2 Double
-mylines = (mconcat $ [square i | i<-[0.1,0.2..1]]) # alignBL
-
-quad :: Side -> Double -> Path V2 Double
-quad x f = (mylines # alignBL :: Path V2 Double) # deform (wibble x f)
-
-tensor :: Diagram B
-tensor = (ff1) # alignBL # strokeP # showOrigin
-    where 
-        f1 = quad LeftSide 0.5
-        ff1 = f1 # deform (wibble LeftSide 2)
-        (d1,(al1,ar1)) = (quad LeftSide 2,(2,1)) -- 
-        (d2,(al2,ar2)) = (quad RightSide 2,(1,2)) --
-        leftDiagram = d1 # shearY (ar2-al2)
-        rightDiagram = d2 # translateY (-al2)
-
-compose :: Diagram B
-compose = (leftDiagram ||| rightDiagram) # alignBL # strokeP # showOrigin
-    where 
-        (d1,(al1,ar1)) = (quad LeftSide 3,(3,1)) -- 
-        (d2,(al2,ar2)) = (quad RightSide 1,(1,1)) --
-        middle = (al1+ar2)/(ar1*2)
-        leftDiagram = d1 # deform (wibble RightSide (middle))
-        rightDiagram = d2 # deform (wibble LeftSide middle)
-
-mainShape :: Diagram B
-mainShape = unitSquare # alignBL <> (tensor)
-
-mainDiagram :: Diagram B
-mainDiagram = unitSquare <> (fst $ drawDiagram example)
-
-main = mainWith mainShape
+main = mainWith $ (path # strokePath :: Diagram B) # frame 1
+    where (path, arity) = drawPath example

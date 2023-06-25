f483ccb637c416d0af6d713ea416a5a1ed9d50ad
Celia Rubio Madrigal <celia_rubio@hotmail.es>
2023-06-25 11:39:13 +0100

    crossings and squarify

--- app/Main.hs
+++ app/Main.hs
@@ -7,11 +7,15 @@ import BrickDiagrams
 example :: BrickDiagram
 example =
     Compose 
-        (Tensor (Tensor (Morphism (1, 2) "0") (Morphism (1, 1) "1")) (Morphism (1, 1) "2")) 
-        (Tensor (Morphism (1, 1) "3") (Morphism (3, 5) "4"))
+        (Tensor (Tensor (Morphism (2, 2) "0") (Morphism (1, 2) "1")) (Morphism (2, 2) "2")) 
+        (Tensor (Morphism (1, 1) "3") (Tensor (Morphism (2, 5) "4") (Crossing 3 (Just f))))
+        where 
+            f 0 = 1
+            f 1 = 2
+            f 2 = 0
 
 main :: IO ()
 main = mainWith $ d # frame 1
     where d = brickToCustom example 
-            # deformCD (Deformation $ scaleY 0.5) 
-            # (customToDiagram "sd")
+            # squarify # scaleCD 0.33
+            # customToDiagram "sd"
--- src/BrickDiagrams.hs
+++ src/BrickDiagrams.hs
@@ -9,17 +9,23 @@ module BrickDiagrams where
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
-data BrickDiagram = Morphism Arity String | Compose BrickDiagram BrickDiagram | Tensor BrickDiagram BrickDiagram
+data BrickDiagram = Crossing Double (Maybe (Double -> Double)) | Morphism Arity String | Compose BrickDiagram BrickDiagram | Tensor BrickDiagram BrickDiagram
 type Arity = (Double, Double)
 
 data Paths = Paths { _bd :: Path V2 Double, _sd :: Path V2 Double }
 data Locatables = Locs { _texts :: [Located (Diagram B)], _boxes :: [Located (Diagram B)] }
-data Properties = Props { _arity :: Arity, _dw :: Double, _warnings :: [String] }
+data Properties = Props { _arity :: Arity, _dw :: Double, _warnings :: String }
 data CustomDiagram = CD { _ps :: Paths, _ls :: Locatables, _pp :: Properties }
 $(makeLenses ''Paths)
 $(makeLenses ''Locatables)
 $(makeLenses ''Properties)
 $(makeLenses ''CustomDiagram)
+
+instance Semigroup Paths where
+    (<>) ps1 ps2 = ps1 # over bd (<> ps2^.bd) # over sd (<> ps2^.sd)
+
+instance Semigroup Locatables where
+    (<>) ls1 ls2 = ls1 # over texts (<> ls2^.texts) # over boxes (<> ls2^.boxes)
 
 drawCubic :: (R1 v, Metric v, Fractional n) => Point v n -> Point v n -> FixedSegment v n
 drawCubic o f = FCubic o (c o f) (c f o) f
@@ -28,7 +34,7 @@ drawCubic o f = FCubic o (c o f) (c f o) f
 -- from Diagrams.Deform (slightly changed)
 approx :: (R1 u, Metric u, Fractional n) => Deformation v u n -> FixedSegment v n -> FixedSegment u n
 approx t (FLinear p0 p1)      = FLinear (deform t p0) (deform t p1)
-approx t (FCubic p0 c1 c2 p1) = drawCubic (deform t p0) (deform t p1)
+approx t (FCubic p0 _ _ p1) = drawCubic (deform t p0) (deform t p1)
 
 -- This is a custom "deformation" for paths such that only the endpoints (and control points) are moved
 deformPath :: (Metric v, Metric u, R1 u, OrderedField n) => Deformation v u n -> Path v n -> Path u n
@@ -60,13 +66,8 @@ alignCD v cd = cd # deformCD (Deformation $ moveOriginTo newOrigin)
 
 joinTwoCD :: (Arity -> Arity -> Arity) -> (Double -> Double -> Double) -> CustomDiagram -> CustomDiagram -> CustomDiagram
 joinTwoCD farity fwidth cd1 cd2 = CD
-    { _ps = Paths 
-        { _bd = cd1^.ps.bd <> cd2^.ps.bd
-        , _sd = cd1^.ps.sd <> cd2^.ps.sd }
-    , _ls = Locs 
-        { _texts = cd1^.ls.texts ++ cd2^.ls.texts
-        , _boxes = cd1^.ls.boxes ++ cd2^.ls.boxes }
-    , _pp = Props 
+    { _ps = cd1^.ps <> cd2^.ps, _ls = cd1^.ls <> cd2^.ls -- Semigroup instances
+    , _pp = Props -- This depends on composition/tensoring
         { _arity = farity (cd1^.pp.arity) (cd2^.pp.arity)
         , _dw = fwidth (cd1^.pp.dw) (cd2^.pp.dw)
         , _warnings = cd1^.pp.warnings ++ cd2^.pp.warnings } }
@@ -74,27 +75,39 @@ joinTwoCD farity fwidth cd1 cd2 = CD
 -- The main constructor of CustomDiagrams
 brickToCustom :: BrickDiagram -> CustomDiagram
 
+-- Only for SD (for BD it gets an empty box)
+brickToCustom (Crossing k mf) = 
+    brickToCustom (Morphism (k,k) "") 
+    # set (ls . boxes) []
+    # set (ps . sd) (toPath pts)
+    where
+        pts = [drawCubic (0 ^& (0.5+i)) (1 ^& (0.5+f i)) | i <-[0..k-1]]
+        f = case mf of 
+                Nothing -> \i -> k-i-1
+                Just jf -> jf
+
 -- A square, pinched in both upper vertices according to its arity
 brickToCustom (Morphism (al,ar) s) = CD
     { _ps = Paths 
         { _bd = unitSquare # alignBL
-        , _sd = toPath [drawCubic ctr pt | pt <- pts] }
+        , _sd = toPath (ptsl++ptsr) }
     , _ls = Locs 
-        { _texts = [Loc ctr (text s # fontSizeG 0.25)]
-        , _boxes = [Loc ctr (square 0.3 # scaleY 1.5 # fc white)] }
+        { _texts = [Loc ctr (text s # fontSizeG 0.25 # translateY (-0.0625))]
+        , _boxes = [Loc ctr (square 0.3 # scaleY 1.5 # fc white)] } -- TODO clip instead
     , _pp = Props 
         { _arity = (1,1), _dw = 1, _warnings = [] } } 
     # pinchCD (-al) # set (pp . arity) (al,1) 
     # pinchCD ar # set (pp . arity) (al,ar)
     where 
-        ctr = p2 (0.5,0.5)
-        pts = [p2 (0,(0.5+i)/al) | i <-[0..al-1]]++[p2 (1,(0.5+i)/ar) | i<-[0..ar-1]]
+        ctr = 0.5 ^& 0.5
+        ptsl = [drawCubic ctr (0 ^& ((0.5+i)/al)) | i <-[0..al-1]]
+        ptsr = [drawCubic ctr (1 ^& ((0.5+i)/ar)) | i <-[0..ar-1]]
 
 -- Composing 2 CustomDiagrams
 brickToCustom (Compose bd1 bd2) = 
     joinTwoCD farity (+) ncd1 ncd2
     # alignCD (-unitX)
-    # over (pp . warnings) ((:) wng)
+    # set (pp . warnings) wng
     where
         [cd1,cd2] = map brickToCustom [bd1,bd2]
         middle = (fst (cd1^.pp.arity) + snd (cd2^.pp.arity))/2
@@ -102,7 +115,7 @@ brickToCustom (Compose bd1 bd2) =
         ncd2 = cd2 # pinchCD (-middle)
         farity (al1,_) (_,ar2) = (al1,ar2)
         wng = if abs (snd (cd1^.pp.arity) - fst (cd2^.pp.arity)) < 0.001 then 
-                "Error while composing" else "" -- give more detail
+                "Error while composing" else "" -- TODO give more detail
 
 -- Tensoring 2 CustomDiagrams
 brickToCustom (Tensor bd1 bd2) = 
@@ -120,13 +133,40 @@ brickToCustom (Tensor bd1 bd2) =
             # alignCD unitY
         farity (al1,ar1) (al2,ar2) = (al1+al2,ar1+ar2)
 
+scaleXCD :: Double -> CustomDiagram -> CustomDiagram
+scaleXCD k cd = cd 
+    # deformCD (Deformation $ scaleX k)
+    # over (pp . dw) (* k)
+
+scaleYCD :: Double -> CustomDiagram -> CustomDiagram
+scaleYCD k cd = cd 
+    # deformCD (Deformation $ scaleY k)
+    # over (pp . arity) (\(al,ar) -> (k*al,k*ar))
+
+scaleCD :: Double -> CustomDiagram -> CustomDiagram
+scaleCD k = (scaleXCD k) . (scaleYCD k)
+
+rectangify :: CustomDiagram -> CustomDiagram
+rectangify cd = cd 
+    # pinchCD (-maxArity) # set (pp.arity) (maxArity,ar) 
+    # pinchCD maxArity # set (pp.arity) (maxArity,maxArity)
+    where
+        (al,ar) = cd^.pp.arity
+        maxArity = max al ar
+
+squarify :: CustomDiagram -> CustomDiagram
+squarify cd = cd
+    # rectangify
+    # scaleXCD (maxArity/(cd^.pp.dw))
+    where
+        (al,ar) = cd^.pp.arity
+        maxArity = max al ar
+
 -- Put together a CustomDiagram into a Diagram B
 customToDiagram :: String -> CustomDiagram -> Diagram B
 customToDiagram tp cd = mconcat diagrams
     where
-        (al,ar) = cd^.pp.arity
-        maxArity = max al ar
-        ncd = cd # pinchCD (-maxArity) # set (pp.arity) (maxArity,ar) # pinchCD maxArity
+        ncd = cd # rectangify
         diagrams = [moveOriginTo (-o) s | (Loc o s) <- ncd^.ls.texts]
             ++ (if tp/="sd" then [ncd^.ps.bd # strokePath] else [])
             ++ (if tp/="bd" then [moveOriginTo (-o) s | (Loc o s) <- ncd^.ls.boxes]
